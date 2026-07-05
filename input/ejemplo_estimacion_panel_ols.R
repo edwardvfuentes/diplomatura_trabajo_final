e739651b90b13e3775147e64a35efc72fe55eabc
Edward <vattico@gmail.com>
2026-07-05 17:21:52 +0200

    Añadimos script de ejemplo de un MCO de Panel con Efectos Fijos

---
+++ input/ejemplo_estimacion_panel_ols.R
@@ -0,0 +1,27 @@
+library(plm)
+library(Formula)
+
+data("EmplUK", package="plm")
+data("Produc", package="plm")
+data("Grunfeld", package="plm")
+data("Wages", package="plm")
+
+
+E <- pdata.frame(EmplUK, index=c("firm","year"), drop.index=TRUE, row.names=TRUE)
+head(E)
+grunf_p <- pdata.frame(Grunfeld, index=c("firm","year"), drop.index=TRUE, row.names=TRUE)
+head(grunf_p)
+
+
+# Ejemplos de fórmulas con paquete Formula
+emp ~ wage + capital | lag(wage, 1) + capital
+emp ~ wage + capital | . -wage + lag(wage, 1)
+
+
+# Ejemplo de estimación de panel de efectos fijos
+grun.fe <- plm(inv~value+capital, data = grunf_p, model = "within")
+
+
+# Si quieres extraer los efectos fijos:
+fixef(grun.fe, type = "dmean")
+summary(fixef(grun.fe, type = "dmean"))
